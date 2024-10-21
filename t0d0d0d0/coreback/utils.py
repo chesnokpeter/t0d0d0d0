d73ce8a51e21bfd7de8c3660e7e2bbff0dbc0452
chesnok <ChesnokovP1107@yandex.ru>
2024-10-21 22:29:15 +0300

    test cryptography

--- t0d0d0d0/coreback/utils.py
+++ t0d0d0d0/coreback/utils.py
@@ -1,6 +1,12 @@
 import random
 import string
-
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 def genAuthCode(length=4):
     characters = string.digits
@@ -8,68 +14,88 @@ def genAuthCode(length=4):
     return random_string
 
 
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import os
-
-# Функция для генерации ключа из Telegram ID
-def generate_key_from_telegram_id(telegram_id):
-    telegram_id_str = str(telegram_id).encode('utf-8')  # Преобразуем ID в байты
-    salt = b'\x00' * 16  # Можно использовать фиксированную соль или генерировать её случайно
+def aes_encrypt(message: str, key: str) -> bytes:
+    key = key.encode('utf-8')
+    salt = b'\x00' * 16
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
-        length=32,  # Длина ключа 256 бит для AES-256
+        length=32,
         salt=salt,
         iterations=100000,
         backend=default_backend()
     )
-    key = kdf.derive(telegram_id_str)
-    return key
-
-# Функция шифрования
-def encrypt_message(telegram_id, message):
-    key = generate_key_from_telegram_id(telegram_id)
-    print(key)
-    
-    # Генерация случайного IV (инициализационный вектор)
+    key = kdf.derive(key)
     iv = os.urandom(16)
-    
-    # Паддинг сообщения до длины, кратной 16 байтам
     padder = padding.PKCS7(128).padder()
     padded_message = padder.update(message.encode('utf-8')) + padder.finalize()
-
-    # Создание шифра AES-CBC
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     ciphertext = encryptor.update(padded_message) + encryptor.finalize()
+    return iv + ciphertext
 
-    return iv + ciphertext  # IV нужно передавать вместе с зашифрованным текстом
-
-# Функция расшифрования
-def decrypt_message(telegram_id, encrypted_message):
-    key = generate_key_from_telegram_id(telegram_id)
-    
-    iv = encrypted_message[:16]
-    ciphertext = encrypted_message[16:]
-    
-    # Создание шифра AES-CBC для расшифрования
+def aes_decrypt(message: str, key: str) -> str:
+    key = key.encode('utf-8')
+    salt = b'\x00' * 16
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(key)
+    iv = message[:16]
+    ciphertext = message[16:]
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     padded_message = decryptor.update(ciphertext) + decryptor.finalize()
-
     unpadder = padding.PKCS7(128).unpadder()
     message = unpadder.update(padded_message) + unpadder.finalize()
-
     return message.decode('utf-8')
 
-telegram_id = 123456789  # Telegram ID (9-11 цифр)
-message = "Привет, как дела?"
+def rsa_generate_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
 
-encrypted_message = encrypt_message(telegram_id, message)
-print("Зашифрованное сообщение:", encrypted_message)
+def rsa_encrypt(public_key, message):
+    ciphertext = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return ciphertext
 
-decrypted_message = decrypt_message(telegram_id, encrypted_message)
-print("Расшифрованное сообщение:", decrypted_message)
+def rsa_decrypt(private_key, ciphertext):
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return plaintext
+
+# telegram_id = 821785013
+# message = "Привет, как дела?"
+
+# encrypted_message = aes_encrypt(message, str(telegram_id))
+# print("Зашифрованное сообщение:", encrypted_message)
+
+# dectypt = aes_decrypt(encrypted_message, str(telegram_id))
+# print('расшифрованное: ', dectypt)
+
+# test = b'hello kak dela'
+# private, public = rsa_generate_keys()
+# enc = rsa_encrypt(public, test)
+# print(enc)
+# dec = rsa_decrypt(private, enc)
+# print(dec)
