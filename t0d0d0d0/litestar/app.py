d1568039b0032debaf021a7a9acb5162dbab5c18
chesnok <ChesnokovP1107@yandex.ru>
2025-01-08 22:27:25 +0300

    beta litestar jwt

--- t0d0d0d0/litestar/app.py
+++ t0d0d0d0/litestar/app.py
@@ -1,4 +1,5 @@
-from litestar import Litestar, post
+from litestar import Litestar, post, Request
+
 
 from dishka.integrations.litestar import setup_dishka, LitestarProvider
 from dishka import make_async_container
@@ -8,9 +9,14 @@ from .ioc import ioc
 
 from .shortcuts import DishkaRouter, after_request, UseCase, SUOW
 
+from typing import Any
+from litestar.di import Provide
+async def di(request: Request):
+    print(request)
 
-@post('/')
-async def generall(user_id: int, s: SUOW, uc: UseCase[TestUserUseCase]) -> str:
+
+@post('/', dependencies={'di': Provide(di)})
+async def generall(user_id: int, s: SUOW, uc: UseCase[TestUserUseCase], di: Any) -> str:
     async with s.uow(uc) as uow:
         r = await uow.uc.execute(user_id)
     return r
