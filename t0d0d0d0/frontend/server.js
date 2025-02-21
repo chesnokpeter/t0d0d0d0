a061f04cc7a3d0fab37c61579bd545661e709f26
chesnok <ChesnokovP1107@yandex.ru>
2025-02-21 22:29:42 +0300

    docker files

---
+++ t0d0d0d0/frontend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 8101;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
