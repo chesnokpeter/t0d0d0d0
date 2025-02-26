698f0c7d99f2b13e4a64f0b8f88b0c2a281bb2f5
chesnok <ChesnokovP1107@yandex.ru>
2025-02-26 15:21:03 +0300

    fix alembic

--- t0d0d0d0/app/infra/__init__.py
+++
@@ -1,5 +0,0 @@
-from .adapters import *
-from .repos import *
-from .uow import UnitOfWork, SetupUOW
-
-__all_realizations_repos__ = [BrokerRepoRabbit, EncryptionRepoHazmat, MemoryRepoRedis, ProjectRepoPostgresql, TaskRepoPostgresql, UserRepoPostgresql]
