2791f658a194e57cad9119427c6713c79e07120e
chesnok <ChesnokovP1107@yandex.ru>
2025-03-12 16:06:06 +0300

    pwa background color fix

--- t0d0d0d0/frontend/vite.config.js
+++ t0d0d0d0/frontend/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         short_name: 't0d0d0d0',
         description: 'minimalistic t0d0 manager',
         theme_color: '#121212',
+        background_color: "#121212",
         icons: [
           {
             src: '192.png',
